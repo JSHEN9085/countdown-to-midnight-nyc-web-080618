1e3c3bb8f926ec5f544bd30182b4b53e3b688e9a
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-26 19:19:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,17 @@
 #write your code here
 
+<<<<<<< HEAD
 def countdown(x)
   x = 10
   while x > 0
   puts "#{x} SECOND(S)!"
   x = x - 1
+=======
+def countdown
+  x = 10
+  while x > 0
+  puts 
+>>>>>>> 0a886f99609f2db63bc04030a9b3141566327f2f
 end
   "HAPPY NEW YEAR!"
 end
